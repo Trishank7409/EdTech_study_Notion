9b7303ebca1486db1f1d31c8ec395d7fb46fdd47
Trishank Sharma <trishank7409@gmail.com>
2024-08-15 12:42:17 +0530

    fix the base url and file names

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+// const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+  // baseURL: BASE_URL,
+});
+
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
